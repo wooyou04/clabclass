e1c916493e46fd8363256f9fcd93805d8b5589ad
wooyou04 <natali@fuutreclass.net>
2020-12-15 18:58:22 +0900

    .

---
+++ helloo.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+    document.getElementById('btn3').addEventListener('click', function(){
+        alert('버튼 3 입니다.')
+    })
+
+document.getElementById('btn2').addEventListener('click',function(){
+    alert('버튼 2 입니다')
+            });
+        }
